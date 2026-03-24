23fa301410c55d910034b83d78463e3698386ba5
fafafariba <seefaribacode@gmail.com>
2026-03-24 18:17:07 -0500

    suppress db logs during tests

--- tests/Api.Tests/CustomWebApplicationFactory.cs
+++ tests/Api.Tests/CustomWebApplicationFactory.cs
@@ -1,6 +1,7 @@
 using Api.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Logging;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -19,6 +20,15 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        builder.UseEnvironment("Testing");
+
+        // Suppress noisy info-level logs (e.g. EF Core SQL) during tests
+        builder.ConfigureLogging(logging =>
+        {
+            logging.ClearProviders();
+            logging.SetMinimumLevel(LogLevel.Warning);
+        });
+
         builder.ConfigureServices(services =>
         {
             // Remove the real DbContext registration
